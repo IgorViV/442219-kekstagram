71da4188eba7a26b0bd2338507522f1b585e7cb5
IgorViV <iupgrader@mail.ru>
2018-12-13 10:41:18 +0300

    Разбивает код на модули

---
+++ js/utilities.js
@@ -0,0 +1,25 @@
+'use strict';
+
+window.utilities = (function () {
+  var ESC_KEYCODE = 27;
+  var ENTER_KEYCODE = 13;
+
+  return {
+    isEscEvent: function (evt, action) {
+      if (evt.keyCode === ESC_KEYCODE) {
+        action();
+      }
+    },
+    isEnterEvent: function (evt, action) {
+      if (evt.keyCode === ENTER_KEYCODE) {
+        action();
+      }
+    },
+    getRandomInteger: function (minNumber, maxNumber) {
+      return Math.floor(minNumber + Math.random() * (maxNumber - minNumber));
+    },
+    getMaxElement: function (arr) {
+      return Math.max.apply(null, arr);
+    }
+  };
+})();
